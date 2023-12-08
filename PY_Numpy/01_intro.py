f75cf286109e569f732eae4a2d477d5aa30dcca8
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2023-12-08 04:00:47 +0000

    Basics of NumPy

---
+++ PY_Numpy/01_intro.py
@@ -0,0 +1,39 @@
+# Numpy is a general-purpose array-processing package. It provides a high-performance multidimensional array object, and tools for working with these arrays. It is the fundamental package for scientific computing with Python.
+# Besides its obvious scientific uses, Numpy can also be used as an efficient multi-dimensional container of generic data.
+
+# Ndarray: NumPy's core data structure is the n-dimensional array, usually referred to as ndarray. NumPy arrays allow fast operations on large amounts of data.
+
+# Basic Operations: NumPy allows you to perform basic mathematical operations on arrays like addition, subtraction, multiplication, division, etc.
+
+# Indexing and Slicing: NumPy arrays can be indexed and sliced like regular Python lists.
+
+# Iterating: NumPy arrays can be iterated over using for loops.
+
+# Conditions and Boolean Arrays: Conditions can be applied to NumPy arrays to create boolean arrays.
+
+# Shape Manipulation: The shape of NumPy arrays can be manipulated using functions like reshape() and ravel().
+
+# Array Manipplication: NumPy provides many functions to manipulate arrays like transpose(), concatenate(), stack(), split(), etc.
+
+# Vectorization: NumPy allows vectorized operations, meaning operations are performed elementwise on whole arrays, making calculations very fast.
+
+# Broadcasting: NumPy supports broadcasting which allows arithmetic operations between arrays of different shapes.
+
+# Structured Arrays: NumPy arrays can store structured data types with fields named tuples.
+
+# Reading and Writing Arrays: NumPy provides functions to save and load arrays from files.
+
+
+# Numpy stands for 'Numerical Python' powerful Python Library that is widely used for scientific computing , data analysis, and numerical computing tasks.
+
+#😎😎😎😎😎😎
+#Installing Numpy 
+# pip install NumPy
+import numpy as np
+myarr = np.array([3,4,5,6])
+
+# In NumPy, an Array is the fundamental object.
+
+#Arrays are used to store HOMOGENEOUS DATA elements in a CONTIGUOUS block of memory.
+
+
