1633304552bd9925b50414bbe1a627f9e0b1617e
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2023-12-23 17:14:16 +0000

    Pandas library in Python

---
+++ PY_Pandas/Pandas_Basics.py
@@ -0,0 +1,34 @@
+# Pandas is an open source data analysis library written in python .
+# It leverages the power and speed of numpy to make data analysis and preprocessing easy for data scientists.
+# It provides rich and highly robust data operations
+
+# Pandas Data Structure:
+# Pandas has two types of data structure :
+# a) Series - Its a one dimensional array with indexes, it stores a single column or row of data in a dataframe.
+# b) Dataframe - It's a tabular spreadsheet like structure representing rows each of which contains one or multiple columns.
+
+# A one-dimensional array capable of holding any type of data -- Series
+# A two-dimensional data structure with columns of potentially different type of data -- DataFrame.
+
+
+
+
+# The Basics:
+# Series: A one-dimensional labeled array capable of holding any data type. It’s like a column in a spreadsheet.
+# DataFrame: A two-dimensional, size-mutable, and heterogeneous tabular data structure with labeled axes (rows and columns). Think of it as a table.
+# Index Objects: These are the row and column labels in pandas. They allow for efficient data retrieval and alignment.
+# Data Manipulation:
+# Reindexing: Changing the order or adding/removing labels in an index.
+# Dropping: Removing rows or columns from a DataFrame.
+# Arithmetic and Data Alignment: Operations between Series and DataFrames align based on index labels.
+# Functions by Element: Applying functions to individual elements.
+# Functions by Row or Column: Applying functions to entire rows or columns.
+# Statistics Functions: Calculating summary statistics like mean, median, etc.
+# Sorting and Ranking: Sorting data by index or values.
+# Correlation and Covariance: Analyzing relationships between variables.
+# “Not a Number” Data: Handling missing or invalid data (NaN).
+# Reading and Writing Data:
+# CSV and Textual Files: Reading and writing data from/to CSV files.
+# HTML Files: Extracting data from HTML tables.
+# XML: Parsing XML data.
+# Microsoft Excel Files: Working with Excel spreadsheets.
